2fc131383416ebbe14e6dc50295e059ad309dae1
Sri Vishnu Totakura <srivishnu@totakura.in>
2019-08-31 08:56:28 +0000

    Fix release script

--- release.sh
+++ release.sh
@@ -9,31 +9,30 @@ version=$1
 if [ -z "$version"  ]; then
   echo "No version passed! Example usage: ./release.sh 1.0.0"
     exit 1
-  fi
-
-  echo "Update version..."
-  old_version=$(grep -o "[0-9]*\.[0-9]*\.[0-9]*" VERSION)
-  sed -i.bak 's/Version = "'$old_version'"/Version = "'$version'"/' VERSION
-  rm VERSION.bak
-  sed -i.bak 's/'$old_version'/'$version'/' README.md
-  rm README.md.bak
-
-  echo "Mark version as released in changelog..."
-  today=$(date +'%Y-%m-%d')
-  sed -i.bak 's/Unreleased/Unreleased\
-    \
-    ## '$version' ('$today')/' CHANGELOG.md
-  rm CHANGELOG.md.bak
-
-  echo "Update contributors..."
-  git contributors | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}' > CONTRIBUTORS
-
-  echo "Update repository..."
-  git add VERSION README.md CHANGELOG.md
-  git commit -m "Bump version to ${version}"
-  git tag --message="v$version" --force "v$version"
-  git tag --message="latest" --force latest
-
-  echo "v$version tagged."
-  echo "Now, run 'git push origin master && git push --tags --force' and publish the release on GitHub."
 fi
+
+echo "Update version..."
+old_version=$(grep -o "[0-9]*\.[0-9]*\.[0-9]*" VERSION)
+sed -i.bak 's/Version = "'$old_version'"/Version = "'$version'"/' VERSION
+rm VERSION.bak
+sed -i.bak 's/'$old_version'/'$version'/' README.md
+rm README.md.bak
+
+echo "Mark version as released in changelog..."
+today=$(date +'%Y-%m-%d')
+sed -i.bak 's/Unreleased/Unreleased\
+  \
+  ## '$version' ('$today')/' CHANGELOG.md
+rm CHANGELOG.md.bak
+
+echo "Update contributors..."
+git contributors | awk '{for (i=2; i<NF; i++) printf $i " "; print $NF}' > CONTRIBUTORS
+
+echo "Update repository..."
+git add VERSION README.md CHANGELOG.md
+git commit -m "Bump version to ${version}"
+git tag --message="v$version" --force "v$version"
+git tag --message="latest" --force latest
+
+echo "v$version tagged."
+echo "Now, run 'git push origin master && git push --tags --force' and publish the release on GitHub."
